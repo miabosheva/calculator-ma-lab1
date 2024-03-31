f1c4510a23a9a791804523b4dc601807a684a56e
miabosheva <boshevam@gmail.com>
2024-03-31 16:41:07 +0200

    Project setup and storyboard design

---
+++ calculator/MyViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MyViewController.swift
+//  calculator
+//
+//  Created by Mia on 3/31/24.
+//
+
+import Foundation
+import UIKit
+
+class MyViewController: UIViewController {
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
